5af0e6c57df1e1c43c2f721acb38d13d1ca3bee2
Diego Meneses <diegoxdc99@hotmail.com>
2018-01-24 14:51:32 -0500

    Finalización del tutorial.

---
+++ app.js
@@ -0,0 +1,6 @@
+var electron = require('electron')
+
+electron.app.on('ready', function () {
+    var mainWindow = new electron.BrowserWindow();
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
